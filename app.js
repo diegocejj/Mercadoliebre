ab51545237f3bf9091eca84b0f54ab304b1cbe84
Diego <diegocej@gmail.com>
2021-09-22 00:34:47 -0300

    initial step. Register css html with express

--- app.js
+++ app.js
@@ -12,6 +12,19 @@ app.get("/", (req, res) => {
     res.sendFile(fileToSend);
 });
 
+app.get('/register',(req,res) => {
+    // ruta del archivo
+    const fileToSend = path.join(__dirname, "/views/register.html");
+    res.sendFile(fileToSend);
+});
+
+/*
+// Redirecciono a la pagina principal
+app.post('/register',(req,res) => {
+    // ruta del archivo
+    res.redirect("/"));
+});
+*/
 
 app.listen(3030, () => {
     console.log(
